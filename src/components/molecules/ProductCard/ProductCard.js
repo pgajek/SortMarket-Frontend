9b308db38d204268e70446bf196a83f7d5a29147
Patryk Gajek <pgajek94@gmail.com>
2021-01-02 12:09:20 +0100

    createOptionsFunction implemented in card component

--- src/components/molecules/ProductCard/ProductCard.js
+++ src/components/molecules/ProductCard/ProductCard.js
@@ -2,16 +2,23 @@ import React, { useState } from "react";
 import "./ProductCard.scss";
 import { ReactComponent as Star } from "assets/icons/star.svg";
 
-const ProductCard = () => {
-  const rating = 4;
+const ProductCard = ({ rating, quantity, unit, name, price, currency }) => {
+  const createSelectItems = () => {
+    let items = [];
+    for (let i = 1; i <= quantity; i++) {
+      items.push(<option value={`${i}`}>{`${i} ${unit}`}</option>);
+    }
+    return items;
+  };
+
   const stars = [1, 2, 3, 4, 5];
   return (
     <div className="card">
       <div className="card__img">
-        <img src="assets/img/test.jpg" alt="" />
+        <img src="assets/img/test.jpg" />
       </div>
-      <h2 className="card__productName">Fajna cegła</h2>
-      <span className="card__productPrice">36.99 PLN</span>
+      <h2 className="card__productName">{name}</h2>
+      <span className="card__productPrice">{`${price} ${currency}`}</span>
       <div className="card__rating">
         {stars.map((star) => (
           <Star
@@ -27,11 +34,7 @@ const ProductCard = () => {
       </a>
       <form className="card__form">
         <select name="quantity" id="quantity" className="card__quantity">
-          <option value="1">1 pcs</option>
-          <option value="2">2 pcs</option>
-          <option value="3">3 pcs</option>
-          <option value="4">4 pcs</option>
-          <option value="5">5 pcs</option>
+          {createSelectItems()}
         </select>
         <button className="btn card__btn">Do koszyka</button>
       </form>
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,12 +1,23 @@
 import "./Root.scss";
 import Nav from "components/organisms/Nav/Nav";
 import Footer from "components/organisms/Footer/Footer";
+import ProductCard from "components/molecules/ProductCard/ProductCard";
 
 function Root() {
   return (
     <div className="root">
-      <Nav />
-      <Footer />
+      {/* <Nav />
+
+      <Footer /> */}
+
+      <ProductCard
+        rating={4}
+        quantity={7}
+        unit="szt"
+        name="Fajna cegła"
+        price={36.66}
+        currency="PLN"
+      />
     </div>
   );
 }
