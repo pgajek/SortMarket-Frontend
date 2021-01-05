18a156ada4b624ecfff7474b563ac6e66f1a3b00
Patryk Gajek <pgajek94@gmail.com>
2021-01-05 16:17:24 +0100

    login panell connected with stroe

--- src/components/molecules/LoginPanel/LoginPanel.js
+++ src/components/molecules/LoginPanel/LoginPanel.js
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
 import "./LoginPanel.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { signin } from "store/actions/userActions";
 
 const LoginPanel = () => {
   const [isLoginOpen, handleLoginOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const userSignin = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(userSignin);
+    dispatch(signin(email, password));
+  };
   return (
     <form
       className={isLoginOpen ? "loginPanel loginPanel--active" : "loginPanel"}
+      onSubmit={submitHandler}
     >
       {isLoginOpen ? (
         <>
           <div className="loginPanel__panelField">
             <label htmlFor="login"></label>
-            <input type="text" name="login" id="login" placeholder="Login" />
+            <input
+              type="text"
+              name="login"
+              id="login"
+              placeholder="Login"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="loginPanel__panelField">
             <label htmlFor="password"></label>
@@ -20,9 +40,13 @@ const LoginPanel = () => {
               name="password"
               id="password"
               placeholder="Hasło"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button className="loginPanel__panelButton btn">Zaloguj</button>
+          <button type="submit" className="loginPanel__panelButton btn">
+            Zaloguj
+          </button>
         </>
       ) : (
         <button className="btn" onClick={() => handleLoginOpen(true)}>
