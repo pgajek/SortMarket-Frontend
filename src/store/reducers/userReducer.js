5d7a6b74ebf641273659a1ff7fa1740aace1bbfe
Patryk Gajek <pgajek94@gmail.com>
2021-01-07 11:18:13 +0100

    removed unnecessary console logs

--- src/store/reducers/userReducer.js
+++ src/store/reducers/userReducer.js
@@ -2,16 +2,25 @@ import {
   USER_SIGNIN_REQUEST,
   USER_SIGNIN_SUCCESS,
   USER_SIGNIN_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "constants/userConstants";
 
 export function userReducer(state = {}, action) {
   switch (action.type) {
     case USER_SIGNIN_REQUEST:
-      return { loading: true };
+      return { ...state, loading: true };
     case USER_SIGNIN_SUCCESS:
-      return { loading: false, userInfo: action.payload };
+      return { ...state, loading: false, token: action.payload.token };
     case USER_SIGNIN_FAIL:
-      return { loading: false, error: action.payload };
+      return { ...state, loading: false, error: action.payload, token: null };
+    case USER_REGISTER_REQUEST:
+      return { ...state, loading: true };
+    case USER_REGISTER_SUCCESS:
+      return { ...state, loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
+      return { ...state, loading: false, error: action.payload };
     default:
       return state;
   }
