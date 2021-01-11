45128877f00749b3084723dcdd46e791fcf92074
Patryk Gajek <pgajek94@gmail.com>
2021-01-11 17:25:43 +0100

    home view fully responsive

--- src/views/Home/HomeScreen.js
+++ src/views/Home/HomeScreen.js
@@ -4,20 +4,111 @@ import Footer from "components/organisms/Footer/Footer";
 import ProductCard from "components/molecules/ProductCard/ProductCard";
 import Slider from "components/organisms/Slider/Slider";
 import Cart from "components/organisms/Cart/Cart";
+import AddBanner from "components/atoms/AddBanner/AddBanner";
+import SearchBar from "components/molecules/SearchBar/SearchBar";
+import PaginationBar from "components/molecules/PaginationBar/PaginationBar";
+import "./HomeScreen.scss";
 const HomeScreen = () => {
   return (
     <>
       <Nav />
       <Slider />
-      {/* <ProductCard
-        rating={4}
-        quantity={7}
-        unit="szt"
-        name="Fajna cegła"
-        price={36.66}
-        currency="PLN"
-        img="assets/img/test.jpg"
-      /> */}
+      <SearchBar />
+      <section className="products">
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <AddBanner />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+        <ProductCard
+          rating={4}
+          quantity={7}
+          unit="szt"
+          name="Fajna cegła"
+          price={36.66}
+          currency="PLN"
+          img="assets/img/test.jpg"
+        />
+      </section>
+      <PaginationBar />
       <Cart />
       <Footer />
     </>
