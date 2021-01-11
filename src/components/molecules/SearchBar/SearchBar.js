11c13cb78712fca94a81e6386d1db2e55516caa7
Patryk Gajek <pgajek94@gmail.com>
2021-01-11 15:56:55 +0100

    searchBar defined

---
+++ src/components/molecules/SearchBar/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { ReactComponent as Loop } from "assets/icons/loop.svg";
+import "./SearchBar.scss";
+const SearchBar = () => {
+  const [search, setSearch] = useState("");
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+  return (
+    <div className="searchBar">
+      <label htmlFor="search" className="searchBar__label"></label>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        className="searchBar__input"
+        type="text"
+        id="search"
+        name="search"
+        placeholder="Search for..."
+      />
+      <button className="searchBar__btn" onClick={(e) => handleClick(e)}>
+        <Loop className="searchBar__loop" />
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
