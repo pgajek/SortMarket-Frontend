fb141f8a66ce14726ee7012976971873fca229eb
Patryk Gajek <pgajek94@gmail.com>
2021-01-26 17:36:44 +0100

    paginationBar button received its functionality

--- src/components/molecules/PaginationBar/PaginationBar.js
+++ src/components/molecules/PaginationBar/PaginationBar.js
@@ -1,25 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "./PaginationBar.scss";
 import { useSelector, useDispatch } from "react-redux";
-import { setProductsOptions } from "store/actions/productsActions";
+import { setProductsOptions, getProducts } from "store/actions/productsActions";
 const PaginationBar = () => {
-  const productsCount = useSelector((state) => state.products.productsCount);
-  const [per_page, setPerPage] = useState(2);
+  const { products, loading, productsCount } = useSelector(
+    (state) => state.products
+  );
+  const [per_page, setPerPage] = useState(10);
   const [offset, setOffset] = useState(0);
-  console.log(offset);
-  const pages = productsCount / per_page;
+  const pages = Math.ceil(productsCount / per_page);
   const [activePage, setActivePage] = useState(1);
+  const dispatch = useDispatch();
+  const [addedCount, setAddedCount] = useState(0);
   const handlePageClick = (e) => {
     const value = e.target.dataset.value;
     setActivePage(value);
     let newOffset;
-    if (value < 3) {
-      newOffset = (value - 1) * 10;
-    } else {
-      newOffset = (value - 1) * 10 + 2;
-    }
+    newOffset = (value - 1) * 10 + addedCount;
+    // if (value < 3) {
+    //   newOffset = (value - 1) * 10;
+    // } else {
+    //   newOffset = (value - 1) * 10 + 2;
+    // }
     setOffset(newOffset);
   };
+  const handleButtonClick = () => {
+    setAddedCount(addedCount + 12);
+    setPerPage(per_page + 12);
+  };
+  useEffect(() => {
+    dispatch(setProductsOptions(offset, per_page));
+    dispatch(getProducts(offset, per_page));
+  }, [offset, per_page]);
   const createPages = () => {
     let items = [];
     for (let i = 1; i <= pages; i++) {
@@ -41,13 +53,16 @@ const PaginationBar = () => {
   return (
     <div className="paginationBar">
       <div className="paginationBar__results">
-        1 - 48 of {productsCount} results
+        {!loading && per_page * activePage - per_page} -
+        {!loading && per_page * activePage} of {productsCount} results
       </div>
       <div className="paginationBar__pages">
         <span className="paginationBar__title">Page:</span>
         {createPages()}
       </div>
-      <button className="btn paginationBar__btn">+ 12 more products</button>
+      <button className="btn paginationBar__btn" onClick={handleButtonClick}>
+        + 12 more products
+      </button>
     </div>
   );
 };
