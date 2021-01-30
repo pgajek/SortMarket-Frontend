b4f8675fb5662d89170172ebd0ba7f8b4136caed
Patryk Gajek <pgajek94@gmail.com>
2021-01-30 13:41:12 +0100

    shipment details form fields

--- src/views/CartView/CartView.js
+++ src/views/CartView/CartView.js
@@ -88,6 +88,72 @@ const ProductScreen = () => {
           </div>
           <div className="cartView__box cartView__shippingDetails">
             <h3 className="cartView__header">Szczegóły wysyłki:</h3>
+            <div className="cartView__field">
+              <label htmlFor="name" className="cartView__label">
+                *Imie:
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="cartView__input"
+              />
+            </div>
+            <div className="cartView__field">
+              <label htmlFor="surname" className="cartView__label">
+                *Nazwisko:
+              </label>
+              <input
+                type="text"
+                id="surname"
+                name="surname"
+                className="cartView__input"
+              />
+            </div>
+            <div className="cartView__field">
+              <label htmlFor="number" className="cartView__label">
+                *Numer telefonu:
+              </label>
+              <input
+                type="text"
+                id="number"
+                name="number"
+                className="cartView__input"
+              />
+            </div>
+            <div className="cartView__field">
+              <label htmlFor="city" className="cartView__label">
+                *Miejscowość:
+              </label>
+              <input
+                type="text"
+                id="city"
+                name="city"
+                className="cartView__input"
+              />
+            </div>
+            <div className="cartView__field">
+              <label htmlFor="adress" className="cartView__label">
+                *Adress:
+              </label>
+              <input
+                type="text"
+                id="adress"
+                name="adress"
+                className="cartView__input"
+              />
+            </div>
+            <div className="cartView__field">
+              <label htmlFor="postalCode" className="cartView__label">
+                *Kod pocztowy:
+              </label>
+              <input
+                type="text"
+                id="postalCode"
+                name="postalCode"
+                className="cartView__input"
+              />
+            </div>
           </div>
           <div className="cartView__box cartView__shippingMethod">
             <h3 className="cartView__header">Metoda wysyłki:</h3>
